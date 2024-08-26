6ed50932b10056a22a8868ca8ad311d50d36fe75
Bryson Tyrrell <bryson.tyrrell@gmail.com>
2024-08-26 15:12:32 -0500

    Initial Commit

---
+++ JamfProAPIClientExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JamfProAPIClientExample
+//
+//  Created by Bryson Tyrrell on 8/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JamfProAPIClientExample/JamfProAPIClientExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  JamfProAPIClientExampleApp.swift
+//  JamfProAPIClientExample
+//
+//  Created by Bryson Tyrrell on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct JamfProAPIClientExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
